e20d12e86d85e46e8ba320adbd670e3cf4f0456b
Kévin Chalet <kevinchalet@gmail.com>
2015-07-07 17:01:40 +0200

    Add a sample application demonstrating how to use the new OpenID 2.0 middleware

---
+++ samples/Mvc.Client/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Http.Authentication;
+using Microsoft.AspNet.Mvc;
+using Mvc.Client.Extensions;
+
+namespace Mvc.Client.Controllers {
+    public class AuthenticationController : Controller {
+        [HttpGet("~/signin")]
+        public IActionResult SignIn() => View("SignIn", Context.GetExternalProviders());
+
+        [HttpPost("~/signin")]
+        public IActionResult SignIn([FromForm] string provider) {
+            // Note: the "provider" parameter corresponds to the external
+            // authentication provider choosen by the user agent.
+            if (string.IsNullOrWhiteSpace(provider)) {
+                return HttpBadRequest();
+            }
+
+            if (!Context.IsProviderSupported(provider)) {
+                return HttpBadRequest();
+            }
+            
+            // Instruct the middleware corresponding to the requested external identity
+            // provider to redirect the user agent to its own authorization endpoint.
+            // Note: the authenticationScheme parameter must match the value configured in Startup.cs
+            return new ChallengeResult(provider, new AuthenticationProperties { RedirectUri = "/" });
+        }
+
+        [HttpGet("~/signout")]
+        [HttpPost("~/signout")]
+        public async Task<IActionResult> SignOut() {
+            // Instruct the cookies middleware to delete the local cookie created
+            // when the user agent is redirected from the external identity provider
+            // after a successful authentication flow (e.g Google or Facebook).
+            await Context.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ samples/Mvc.Client/Startup.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.AspNet.Authentication;
+using Microsoft.AspNet.Authentication.Cookies;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+
+namespace Mvc.Client {
+    public class Startup {
+        public void ConfigureServices(IServiceCollection services) {
+            services.Configure<ExternalAuthenticationOptions>(options => {
+                options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            });
+
+            services.AddAuthentication();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+            var factory = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            factory.AddConsole();
+
+            app.UseStaticFiles();
+
+            app.UseCookieAuthentication(options => {
+                options.AutomaticAuthentication = true;
+                options.AuthenticationScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.LoginPath = new PathString("/signin");
+            });
+
+            app.UseOpenIdAuthentication("Orange", options => {
+                options.AuthenticationScheme = "Orange";
+                options.Caption = "Orange";
+                options.Authority = new Uri("http://orange.fr/");
+                options.CallbackPath = new PathString("/signin-orange");
+            });
+
+            app.UseOpenIdAuthentication("StackExchange", options => {
+                options.AuthenticationScheme = "StackExchange";
+                options.Caption = "StackExchange";
+                options.Authority = new Uri("https://openid.stackexchange.com/");
+                options.CallbackPath = new PathString("/signin-stackexchange");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
