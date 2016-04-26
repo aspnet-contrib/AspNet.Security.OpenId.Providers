b6834195a8ffdb497cffaf9d9196566cffebdafd
Kévin Chalet <kevinchalet@gmail.com>
2016-04-26 16:47:07 +0200

    Remove the forwarded headers middleware

--- samples/Mvc.Client/Startup.cs
+++ samples/Mvc.Client/Startup.cs
@@ -9,7 +9,6 @@ using AspNet.Security.OpenId;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Mvc.Client {
@@ -23,10 +22,6 @@ namespace Mvc.Client {
         }
 
         public void Configure(IApplicationBuilder app) {
-            app.UseForwardedHeaders(new ForwardedHeadersOptions {
-                ForwardedHeaders = ForwardedHeaders.All
-            });
-
             app.UseStaticFiles();
 
             app.UseCookieAuthentication(new CookieAuthenticationOptions {
