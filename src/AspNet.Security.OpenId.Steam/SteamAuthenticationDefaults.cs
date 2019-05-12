8d6324e7c7eb76867a2a76e883795cc38c212bb9
martincostello <martin@martincostello.com>
2019-05-12 13:55:18 +0100

    Remove redundant using
    Remove unused using statement.

--- src/AspNet.Security.OpenId.Steam/SteamAuthenticationDefaults.cs
+++ src/AspNet.Security.OpenId.Steam/SteamAuthenticationDefaults.cs
@@ -4,7 +4,6 @@
  * for more information concerning the license and the contributors participating to this project.
  */
 
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Authentication;
 
 namespace AspNet.Security.OpenId.Steam
