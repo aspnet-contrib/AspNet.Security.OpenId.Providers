effbfa31eec969779b800f33ac1fd1b4668a4a64
martincostello <martin@martincostello.com>
2019-05-25 12:51:33 +0100

    Add integration tests
    Add integration tests for the providers.
    Update the JetBrains package to the latest version.

---
+++ test/AspNet.Security.OpenId.Providers.Tests/Steam/SteamTests.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OpenId.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AspNet.Security.OpenId.Steam
+{
+    public class SteamTests : OpenIdTests<SteamAuthenticationOptions>
+    {
+        public SteamTests(ITestOutputHelper outputHelper)
+        {
+            OutputHelper = outputHelper;
+            Interceptor.ThrowOnMissingRegistration = true;
+
+            UserAttributes["email"] = "john@john-smith.local";
+            UserAttributes["first"] = "John";
+            UserAttributes["namePerson"] = "John Smith";
+        }
+
+        public override string DefaultScheme => SteamAuthenticationDefaults.AuthenticationScheme;
+
+        public override string UserIdentity => "https://steamcommunity.com/openid/id/my-id";
+
+        protected internal override void RegisterAuthentication(AuthenticationBuilder builder)
+        {
+            builder.AddSteam(options =>
+            {
+                ConfigureDefaults(builder, options);
+                options.ApplicationKey = "steam-application-key";
+            });
+        }
+
+        [Theory]
+        [InlineData(ClaimTypes.NameIdentifier, "https://steamcommunity.com/openid/id/my-id")]
+        [InlineData(ClaimTypes.Name, "John Smith")]
+        [InlineData(ClaimTypes.Email, "john@john-smith.local")]
+        [InlineData(ClaimTypes.GivenName, "John")]
+        public async Task Can_Sign_In_Using_Steam(string claimType, string claimValue)
+        {
+            // Arrange
+            using (var server = CreateTestServer())
+            {
+                // Act
+                var claims = await AuthenticateUserAsync(server);
+
+                // Assert
+                AssertClaim(claims, claimType, claimValue);
+            }
+        }
+    }
+}
