fe6767cf74d1a80cc3dee89f5dc5b178123c2ba0
martincostello <martin@martincostello.com>
2019-06-01 11:45:48 +0100

    Use latest Bootstrap and jQuery
    Update to latest Bootstrap and jQuery versions.
    Sort provider names.
    Improve HTML.

--- samples/Mvc.Client/Controllers/AuthenticationController.cs
+++ samples/Mvc.Client/Controllers/AuthenticationController.cs
@@ -15,10 +15,7 @@ namespace Mvc.Client.Controllers
     public class AuthenticationController : Controller
     {
         [HttpGet("~/signin")]
-        public async Task<IActionResult> SignIn()
-        {
-            return View("SignIn", await HttpContext.GetExternalProvidersAsync());
-        }
+        public async Task<IActionResult> SignIn() => View("SignIn", await HttpContext.GetExternalProvidersAsync());
 
         [HttpPost("~/signin")]
         public async Task<IActionResult> SignIn([FromForm] string provider)
