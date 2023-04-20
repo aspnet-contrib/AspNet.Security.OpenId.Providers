1d81868a45d463c7ac49b7b17ca4654ffb2a5761
Roger <58123692+xRoier@users.noreply.github.com>
2023-04-20 23:25:25 +0200

    Fix typo, steam response is "steamid", not steamids.

--- src/AspNet.Security.OpenId.Steam/SteamAuthenticationConstants.cs
+++ src/AspNet.Security.OpenId.Steam/SteamAuthenticationConstants.cs
@@ -17,7 +17,7 @@ public static class SteamAuthenticationConstants
     public static class Parameters
     {
         public const string Key = "key";
-        public const string SteamId = "steamids";
+        public const string SteamId = "steamid";
         public const string Response = "response";
         public const string Players = "players";
         public const string Name = "personaname";
